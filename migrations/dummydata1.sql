97a06b45e5b21bcae344e91b7b89c1c66cbcbea2
tolmaeli <144253873+tolmaeli@users.noreply.github.com>
2025-02-26 15:40:49 -0700

    Update dummydata1.sql

--- migrations/dummydata1.sql
+++ migrations/dummydata1.sql
@@ -2,8 +2,8 @@
 INSERT INTO VideoGame (Price, GameName, Rating, Genre, Quantity, Company)
 VALUES 
   (59.99, 'The Legend of Zelda: Breath of the Wild', 5, 'Adventure', 100, 'Nintendo'),
-  (49.99, 'Halo Infinite', 4, 'FPS', 75, 'Xbox Game Studios');
-
+  (49.99, 'Halo Infinite', 4, 'FPS', 75, 'Xbox Game Studios'),
+  (59.99, 'Super Smash Bros. Ulitmate', 5, 'Fighting'. 50, 'Nintendo');
 -- Insert into Retailor
 INSERT INTO Retailor (RPassword, RUserName)
 VALUES 
@@ -20,16 +20,19 @@ VALUES
 INSERT INTO Updates (GameID, StaffID)
 VALUES 
   (1, 1),
-  (2, 1);
+  (2, 1),
+  (3, 1);
 
 -- Insert into Views
 INSERT INTO Views (GameID, RetailID)
 VALUES 
   (1, 1),
-  (2, 2);
+  (2, 2),
+  (3, 3);
 
 -- Insert into VideoGame_Platform
 INSERT INTO VideoGame_Platform (Platform, GameID)
 VALUES 
   ('Nintendo Switch', 1),
-  ('Xbox Series X', 2);
+  ('Xbox Series X', 2)
+  ('Nintendo Switch', 3);

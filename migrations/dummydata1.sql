4cd3b1ae9003d1c3e06e529f278601b0f2dea008
ethanasp <124311855+littlerocker77@users.noreply.github.com>
2025-03-17 10:54:40 -0600

    Update dummydata1.sql
    Fixed minor syntax errors

--- migrations/dummydata1.sql
+++ migrations/dummydata1.sql
@@ -3,15 +3,16 @@ INSERT INTO VideoGame (Price, GameName, Rating, Genre, Quantity, Company)
 VALUES 
   (59.99, 'The Legend of Zelda: Breath of the Wild', 5, 'Adventure', 100, 'Nintendo'),
   (49.99, 'Halo Infinite', 4, 'FPS', 75, 'Xbox Game Studios'),
-  (59.99, 'Super Smash Bros. Ulitmate', 5, 'Fighting'. 50, 'Nintendo');
+  (59.99, 'Super Smash Bros. Ultimate', 5, 'Fighting', 50, 'Nintendo');  -- Fixed typo and misplaced period
+
 -- Insert into Retailor
 INSERT INTO Retailor (RPassword, RUserName)
 VALUES 
   ('retail123', 'retail_user1'),
   ('retail456', 'retail_user2');
 
--- Insert into WearhouseStaff
-INSERT INTO WearhouseStaff (SPassword, SUserName, IsAdmin)
+-- Insert into WarehouseStaff (Fixed Typo)
+INSERT INTO WarehouseStaff (SPassword, SUserName, IsAdmin)
 VALUES 
   ('staff123', 'admin_john', TRUE),
   ('staff456', 'worker_emma', FALSE);
@@ -23,16 +24,15 @@ VALUES
   (2, 1),
   (3, 1);
 
--- Insert into Views
+-- Insert into Views (Fixed RetailID 3 issue)
 INSERT INTO Views (GameID, RetailID)
 VALUES 
   (1, 1),
-  (2, 2),
-  (3, 3);
+  (2, 2);
 
--- Insert into VideoGame_Platform
+-- Insert into VideoGame_Platform (Fixed missing comma)
 INSERT INTO VideoGame_Platform (Platform, GameID)
 VALUES 
   ('Nintendo Switch', 1),
-  ('Xbox Series X', 2)
+  ('Xbox Series X', 2),
   ('Nintendo Switch', 3);

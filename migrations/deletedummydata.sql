fefcadd72940fca0a4206079d4d64fc6efa2019f
ethanasp <124311855+littlerocker77@users.noreply.github.com>
2025-03-18 11:51:03 -0600

    Update deletedummydata.sql

--- migrations/deletedummydata.sql
+++ migrations/deletedummydata.sql
@@ -1,15 +1,14 @@
 SET SQL_SAFE_UPDATES = 0;
--- Delete in reverse order to respect foreign keys
+
+-- Delete in reverse order to respect foreign key constraints
 DELETE FROM VideoGame_Platform;
 DELETE FROM Views;
 DELETE FROM Updates;
-DELETE FROM WarehouseStaff; 
-DELETE FROM Retailor;
+DELETE FROM UserTable;  -- Combines WarehouseStaff & Retailor
 DELETE FROM VideoGame;
 
 -- Reset auto-increment counters (optional)
 ALTER TABLE VideoGame AUTO_INCREMENT = 1;
-ALTER TABLE Retailor AUTO_INCREMENT = 1;
-ALTER TABLE WarehouseStaff AUTO_INCREMENT = 1;
+ALTER TABLE UserTable AUTO_INCREMENT = 1;
 
 SET SQL_SAFE_UPDATES = 1;

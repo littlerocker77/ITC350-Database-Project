79e4a3af5931266ce7b6a440cd63415669e188c2
efrainr7 <144161492+efrainr7@users.noreply.github.com>
2025-03-31 11:47:26 -0600

    Update dummydata1.sql

--- migrations/dummydata1.sql
+++ migrations/dummydata1.sql
@@ -1,3 +1,10 @@
+-- Insert into VideoGame_Platform (Associates games with platforms)
+INSERT INTO VideoGame_Platform (Platform, PlatformID)
+VALUES 
+  ('Nintendo Switch', 1),
+  ('Xbox Series X', 2),
+  ('PlayStation 5', 3);
+
 -- Dummy data of various video game attributes that will be used in the application
 -- Insert into VideoGame
 INSERT INTO VideoGame (Price, GameName, Rating, Genre, Quantity, PlatformID)
@@ -6,6 +13,12 @@ VALUES
   (49.99, 'Halo Infinite', 4, 'FPS', 75, 2),
   (59.99, 'Astro Bot', 5, 'Platformer', 50, 3);
 
+-- Insert into Updates (Tracks which staff updated which game)
+INSERT INTO Updates (GameID, StaffID)
+VALUES 
+  (1, 3),  -- Assuming 'admin_john' has UserID 3
+  (2, 3),
+  (3, 3);
 
 -- Insert into UserTable (Retailers and Warehouse Staff)
 -- Retailers (UserType = 1)
@@ -20,22 +33,4 @@ VALUES
   ('staff123', 'admin_john', 0),
   ('staff456', 'worker_emma', 0);
 
--- Insert into Updates (Tracks which staff updated which game)
-INSERT INTO Updates (GameID, StaffID)
-VALUES 
-  (1, 3),  -- Assuming 'admin_john' has UserID 3
-  (2, 3),
-  (3, 3);
 
--- Insert into Views (Tracks which retailers viewed which games)
-INSERT INTO Views (GameID, UserID)
-VALUES 
-  (1, 1),  -- retail_user1 viewed The Legend of Zelda
-  (2, 2);  -- retail_user2 viewed Halo Infinite
-
--- Insert into VideoGame_Platform (Associates games with platforms)
-INSERT INTO VideoGame_Platform (Platform, PlatformID)
-VALUES 
-  ('Nintendo Switch', 1),
-  ('Xbox Series X', 2),
-  ('PlayStation 5', 3);
